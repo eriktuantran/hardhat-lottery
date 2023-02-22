89ae7d7214b2bca7a7b269f5de9764058704ef69
Tuan Tran <quoctuanuit@gmail.com>
2023-02-22 21:45:33 +0100

    Added namedaccounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,4 +8,12 @@ require("@nomiclabs/hardhat-waffle")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.17",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
