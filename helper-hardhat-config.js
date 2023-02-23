f64048e98c4d8a97fd8bcac8ffe54a69b5ef109f
Tuan Tran <quoctuanuit@gmail.com>
2023-02-23 06:57:53 +0100

    helper config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,14 +1,16 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     5: {
         name: "goerli",
-        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
-    },
-    137: {
-        name: "matic",
-        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     },
     31337: {
-        name: "localhost",
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     },
 }
 const developmentChains = ["hardhat", "localhost"]
