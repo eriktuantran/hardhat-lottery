dd9329921c97913b864312a4972ffd91c307ff90
Tuan Tran <quoctuanuit@gmail.com>
2023-02-23 06:57:34 +0100

    deploy raffle param cont

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,35 @@
 const { network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+
+const VRF_SUBSCRIPTION_FUND_AMOUNT = ethers.utils.parseEther("10")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContracts("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+
+        // Create subscription which is by website in the testnet
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1) // Wait for 1 confirmation
+        subscriptionId = transactionReceipt.events[0].args.subId
+
+        // Fund subscription
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUBSCRIPTION_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane]
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmation: network.config.blockConfirmations || 1,
     })
