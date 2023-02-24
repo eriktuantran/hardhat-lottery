8e6b9717d15fb7f00f917f52af0bf475025b634b
Tuan Tran <quoctuanuit@gmail.com>
2023-02-24 21:49:07 +0100

    first unit test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,13 +1,23 @@
-const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
-const { developmentChains } = require("../../helper-hardhat-config")
+const { assert } = require("chai")
+const { getNamedAccounts, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle", async function () {
           let raffle, vrfCoordinatorV2Mock
+          const chainId = network.config.chainId
           before(async function () {
-              const [deployer] = await getNamedAccounts()
+              const { deployer } = await getNamedAccounts()
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+          })
+          describe("constructor", async function () {
+              it("initializes the raffle correctly", async function () {
+                  const raffleState = await raffle.getRaffleState()
+                  const interval = await raffle.getInterval()
+                  assert.equal(raffleState.toString(), "0")
+                  assert.equal(interval.toString(), networkConfig[chainId].interval)
+              })
           })
       })
