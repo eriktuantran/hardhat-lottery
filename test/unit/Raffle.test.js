d4c7782a558b1270e590c591d7f3480b7e0a6877
Tuan Tran <quoctuanuit@gmail.com>
2023-02-24 21:38:47 +0100

    add unit test before each

---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,13 @@
+const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle", async function () {
+          let raffle, vrfCoordinatorV2Mock
+          before(async function () {
+              const [deployer] = await getNamedAccounts()
+              await deployments.fixture(["all"])
+              raffle = await ethers.getContract("Raffle", deployer)
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+          })
+      })
