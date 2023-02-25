6f0345064d061bda2210910d7c53cc9e4706faf9
Tuan Tran <quoctuanuit@gmail.com>
2023-02-25 12:05:03 +0100

    added test cases

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -6,7 +6,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
     : describe("Raffle", async function () {
           let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, interval, deployer, subscriptionId
           const chainId = network.config.chainId
-          before(async function () {
+          beforeEach(async function () {
               deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
@@ -15,7 +15,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               interval = await raffle.getInterval()
               subscriptionId = await raffle.getSubscriptionId()
           })
-          describe("constructor", async function () {
+          describe("constructor", function () {
               it("initializes the raffle correctly", async function () {
                   const raffleState = await raffle.getRaffleState()
                   const interval = await raffle.getInterval()
@@ -49,14 +49,34 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   await network.provider.send("evm_mine", [])
                   // we pretend to be a keeper for a second
 
-                  console.log(`subscriptionId ${subscriptionId} ${raffle.address}`)
                   await vrfCoordinatorV2Mock.addConsumer(subscriptionId.toNumber(), raffle.address)
-
                   await raffle.performUpkeep([]) // changes the state to calculating for our comparison below
                   await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
                       // is reverted as raffle is calculating
                       "Raffle__NotOpen"
                   )
+              })
+          })
+          describe("Checkupkeep", function () {
+              it("returns false if people have not sent any eth", async () => {
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
+                  assert(!upkeepNeeded)
+              })
+              it("return false if raffle is not open", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  // for a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  // we pretend to be a keeper for a second
+                  console.log(`subscriptionId ${subscriptionId} ${raffle.address}`)
+                  await vrfCoordinatorV2Mock.addConsumer(subscriptionId.toNumber(), raffle.address)
+                  await raffle.performUpkeep([]) // changes the state to calculating for our comparison below
+                  const raffleState = await raffle.getRaffleState()
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
+                  assert.equal(raffleState.toString(), "1")
+                  assert(!upkeepNeeded)
               })
           })
       })
