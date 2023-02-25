d57d3bb5481ad9066ed7986c90a73f943b42d96a
Tuan Tran <quoctuanuit@gmail.com>
2023-02-25 11:11:16 +0100

    test run success

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,16 +1,19 @@
-const { assert } = require("chai")
-const { getNamedAccounts, deployments, ethers } = require("hardhat")
+const { deployments, ethers, getNamedAccounts, network } = require("hardhat")
+const { assert, expect } = require("chai")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle", async function () {
-          let raffle, vrfCoordinatorV2Mock
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, interval, deployer, subscriptionId
           const chainId = network.config.chainId
           before(async function () {
-              const { deployer } = await getNamedAccounts()
+              deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
+              subscriptionId = await raffle.getSubscriptionId()
           })
           describe("constructor", async function () {
               it("initializes the raffle correctly", async function () {
@@ -18,6 +21,42 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   const interval = await raffle.getInterval()
                   assert.equal(raffleState.toString(), "0")
                   assert.equal(interval.toString(), networkConfig[chainId].interval)
+              })
+          })
+          describe("enterRaffle", function () {
+              it("reverts when you don't pay enough", async () => {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith(
+                      // is reverted when not paid enough or raffle is not open
+                      "Raffle__NotEnoughEthEnter"
+                  )
+              })
+              it("records player when they enter", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayer(0)
+                  assert.equal(playerFromContract, deployer)
+              })
+              it("emits event on enter", async () => {
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
+                      // emits RaffleEnter event if entered to index player(s) address
+                      raffle,
+                      "RaffleEnter"
+                  )
+              })
+              it("doesn't allow entrance when raffle is calculating", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  // for a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  // we pretend to be a keeper for a second
+
+                  console.log(`subscriptionId ${subscriptionId} ${raffle.address}`)
+                  await vrfCoordinatorV2Mock.addConsumer(subscriptionId.toNumber(), raffle.address)
+
+                  await raffle.performUpkeep([]) // changes the state to calculating for our comparison below
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
+                      // is reverted as raffle is calculating
+                      "Raffle__NotOpen"
+                  )
               })
           })
       })
