7300443f9fc365961516b84cab66b2b2a04e43d7
Tuan Tran <quoctuanuit@gmail.com>
2023-02-25 11:10:48 +0100

    fix version for mock VRF

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.7;
 
-import "../../node_modules/@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
