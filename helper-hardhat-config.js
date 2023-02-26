98426925d928b3073b0757908b0e1b2be22a3292
Tuan Tran <quoctuanuit@gmail.com>
2023-02-26 14:02:06 +0100

    localhost environment

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -10,12 +10,20 @@ const networkConfig = {
         callbackGasLimit: 500000,
         interval: 30,
     },
-    31337: {
+    default: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: 500000,
         interval: 30,
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        interval: "30",
+        entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
     },
 }
 const developmentChains = ["hardhat", "localhost"]
